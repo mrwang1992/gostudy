e5f07552cb24b71dc54fc8fe0cd2f69fe2c6fd63
wanghuaiting <wanghuaiting@baidu.com>
2017-06-25 16:40:46 +0800

    up for demo redis pool

--- otherlib/testredis/pool01/main.go
+++ otherlib/testredis/pool01/main.go
@@ -1,5 +1,8 @@
 package main
 
+// demo for using redisgo pool
+// from blog: http://blog.csdn.net/u010471121/article/details/52779039
+
 import (
 	"flag"
 	"fmt"
@@ -13,16 +16,23 @@ var (
 	redisPassword = flag.String("redisPassword", "", "")
 )
 
+// init redis pool
 func newPool(server, password string) *redis.Pool {
 	return &redis.Pool{
-		MaxIdle:     3,
-		MaxActive:   5,
+		// max redis conn idle
+		MaxIdle: 3,
+		// max redis conn num
+		MaxActive: 5,
+		// max idle wait time for redis conn
 		IdleTimeout: 240 * time.Second,
+		// make redis conn function
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", server)
 			if err != nil {
 				return nil, err
 			}
+
+			// use for auth
 			/*
 				if _, err := c.Do("AUTH", password); err != nil {
 					c.Close()
@@ -31,6 +41,7 @@ func newPool(server, password string) *redis.Pool {
 			*/
 			return c, err
 		},
+		// test conn is alive function
 		TestOnBorrow: func(c redis.Conn, t time.Time) error {
 			if time.Since(t) < time.Minute {
 				return nil
